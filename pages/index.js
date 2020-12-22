316038a9aaeca189bcc714d28351b87e637c26e5
Glauber Souza <Glauber.Souza@keyruscorp.key>
2020-12-22 17:53:27 -0300

    home node

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import { useState } from "react"
+
+function Home(){
+    return <div>
+                <h3>Home</h3>
+               <Contador />
+                
+            </div>
+        }
+
+function Contador() {
+    const [contador,setContador] = useState(1);
+
+    function adicionarContador () {
+        setContador(contador + 1);
+    }
+    return(
+        <div>
+            <div>{Contador}</div>
+            <button onClick={adicionarContador}>adicionarContador</button>
+        </div>
+    )
+}
+
+export default Home
